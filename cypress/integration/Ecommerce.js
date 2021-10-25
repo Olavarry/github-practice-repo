95ae1a52a168eceb2e5dcda1892d00434eff7311
Jesus <jschz89.sm@gmail.com>
2021-10-25 10:48:12 -0500

    Se agrega nueva pagina de prueba y se modifica el package.json

---
+++ cypress/integration/Ecommerce.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+
+beforeEach(() => {
+    cy.visit('https://abstracta.us/industries/ecommerce')
+  })
+
+  it('validate title', () => {
+    cy.get('h1').should('have.text', 'E-commerce Testing Services')
+      
+  })
+  
