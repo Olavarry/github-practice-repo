c5c78d5e8a5d0b5fbcab86858cf3fcbb1b45311b
Luis Olavaria <kukiluis89@gmail.com>
2025-01-09 13:46:58 -0600

    handle exceptions

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,33 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// a method to help us reduce noise, some of these errors are not valid
+// MCB suggested adding logs to analyze which exceptions may have valid data
+// log is temp until we figure out a solution of using cy.log inside Cypress.on
+// Learn more about uncaught exception handling here - https://docs.cypress.io/api/events/catalog-of-events#Uncaught-Exceptions
+
+Cypress.Commands.add('ignoreUncaughtException', (accountId, pagePath, log = 'cy') => {
+	const resizeObserverError = /^[^(ResizeObserver loop limit exceeded)]/;
+
+	Cypress.on('uncaught:exception', (err, runnable) => {
+		const errorMessage = {
+			accountId,
+			pagePath,
+			error: err
+		};
+		/* returning false here prevents Cypress from failing the mobile tests */
+		if (resizeObserverError.test(err.message)) {
+			return false;
+		}
+		if (log === 'cy') {
+			cy.log(errorMessage);
+		} else {
+			console.log(errorMessage);
+		}
+		return false;
+	});
+});
+
+//  If you need to catch the uncaught:exception error then use this command.
+Cypress.Commands.add('catchUncaughtException', () => {
+	Cypress.on('uncaught:exception', (err, runnable) => {
+		throw err;
+	});
+});
